2b203137cfbf23d6fe01557644e245f243140841
Robby <77552033+TinyRobby@users.noreply.github.com>
2021-05-12 11:16:43 -0400

    Update icecream.cpp

--- icecream.cpp
+++ icecream.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
-#include <fstream>
+
+using namespace std;
+
+int main( int argc, char *argv[] ) {
+        if ( argc != 2 )
+        {
+            cout << "usage: " << argv[0] << " <option> <flavor>\n" << endl;
+        }
+        if ( argc == 2 )
+        {
+          if(argv[1] == "--flavor") {
+             if(argv[2] == "ged") {
+               system("git clone https://github.com/TinyRobby/ged");
+             }
+            
+          }
+  return 0;          
+}
